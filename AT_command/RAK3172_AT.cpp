5af79b4ddff20b52fb02e8edef5d89b45f245f65
Canopus <49629370+NamNamIoT@users.noreply.github.com>
2023-08-02 09:53:21 +0700

    Update RAK3172_AT.cpp

--- AT_command/RAK3172_AT.cpp
+++ AT_command/RAK3172_AT.cpp
@@ -46,45 +46,19 @@ bool RAK3172::rk_switchToRecv() {
 }
 
 String RAK3172::rk_getLoRaStatus() {
-  sendRawCommand(F("at+get_config=lora:status"));
-  ret = rk_recvData();
-  ret.trim();
-  return ret;
+  return false;
 }
 
 String RAK3172::rk_getChannelList() {
-  sendRawCommand(F("at+get_config=lora:channel"));
-  ret = rk_recvData();
-  ret.trim();
-  return ret;
+  return false;
 }
 
 bool RAK3172::rk_setRate(int rate) {
-  sendRawCommand("at+set_config=lora:dr:" + (String)rate);
-  ret = rk_recvData();
-  if (ret.indexOf("OK") >= 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return false;
 }
 
 bool RAK3172::rk_setClass(int classMode) {
-  if (classMode > 2) {
-    _serial1.println(F("Parameter error"));
-    return false;
-  }
-  command = "at+set_config=lora:class:" + (String)classMode;
-  sendRawCommand(command);
-  ret = rk_recvData();
-#if defined DEBUG_MODE
-  _serial1.println(ret);
-#endif
-  if (ret.indexOf("OK") >= 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return false;
 }
 
 bool RAK3172::rk_setRegion(int region) {
@@ -126,50 +100,19 @@ bool RAK3172::rk_setRegion(int region) {
       REGION = "US915_Hybrid";
       break;
   }
-  _serial1.println("Current work region: " + REGION);
-  sendRawCommand("at+set_config=lora:region:" + REGION);
-  ret = rk_recvData();
-#if defined DEBUG_MODE
-  _serial1.println(ret);
-#endif
-  if (ret.indexOf("OK") >= 0) {
-    return true;
-  } else {
-    return false;
-  }
+return false;
 }
 
 void RAK3172::rk_sleep(int mode) {
-  switch (mode) {
-    case 1:
-      sendRawCommand(F("at+set_config=device:sleep:1"));
-      break;
-    case 0:
-      sendRawCommand(F("at+set_config=device:sleep:0"));
-      break;
-    default:
-      _serial1.println(F("Parameter error"));
-      break;
-  }
+  return false;
 }
 
 bool RAK3172::rk_setSendinterval(int mode, int value) {
-  if (mode > 2) {
-    _serial1.println("The mode set Error,the mode is '0','1' or '2'.");
-    return false;
-  }
-
-  sendRawCommand("at+set_config=lora:send_interval:" + (String)mode + ":" + (String)value);
-  ret = rk_recvData();
-  if (ret.indexOf("OK") >= 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return false;
 }
 
 void RAK3172::rk_reset(void) {
-  sendRawCommand(F("at+set_config=device:restart"));
+  return false;
 }
 
 bool RAK3172::rk_setWorkingMode(int mode) {
@@ -196,146 +139,23 @@ bool RAK3172::rk_setWorkingMode(int mode) {
 }
 
 bool RAK3172::rk_setJoinMode(int mode) {
-  switch (mode) {
-    case 0:
-      sendRawCommand(F("at+set_config=lora:join_mode:0"));  //join Network through OTAA mode.
-      break;
-    case 1:
-      sendRawCommand(F("at+set_config=lora:join_mode:1"));  //join Network through ABP mode.
-      break;
-    default:
-      return false;
-  }
-  ret = rk_recvData();
-  if (ret.indexOf("OK") >= 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return false;
 }
 
 bool RAK3172::rk_joinLoRaNetwork(int timeout) {
-  String command = "at+join";
-  //  _serial1.println(command);
-  sendRawCommand(command);
-  ret = rk_recvData();
-  if (ret != NULL) {
-    if (ret.indexOf("OK") >= 0)
-      return true;
-    else if (ret.indexOf("ERROR") >= 0)
-      return false;
-  }
-  for (int i = 0; i <= timeout / 2; i++) {
-    ret = rk_recvData();
-    if (ret != NULL) {
-      _serial1.print("<- " + ret);
-      if (ret.indexOf("OK") >= 0)
-        return true;
-      // else if(ret.indexOf("ERROR") >= 0)
-      //   return false;
-    }
-  }
-
   return false;
 }
 
 bool RAK3172::rk_initOTAA(String devEUI, String appEUI, String appKEY) {
-  // String command = "";
-  if (devEUI.length() == 16) {
-    _devEUI = devEUI;
-  } else {
-    _serial1.println("The parameter devEUI is set incorrectly!");
-  }
-  if (appEUI.length() == 16) {
-    _appEUI = appEUI;
-  } else {
-    _serial1.println("The parameter appEUI is set incorrectly!");
-  }
-  if (appKEY.length() == 32) {
-    _appKEY = appKEY;
-  } else {
-    _serial1.println("The parameter appKEY is set incorrectly!");
-  }
-
-  command = "at+set_config=lora:dev_eui:" + _devEUI;
-  //  _serial1.println(command);
-  sendRawCommand(command);
-  ret = rk_recvData();
-  if (ret.indexOf("OK") >= 0) {
-    command = "at+set_config=lora:app_eui:" + _appEUI;
-    //  _serial1.println(command);
-    sendRawCommand(command);
-
-    ret = rk_recvData();
-    if (ret.indexOf("OK") >= 0) {
-      command = "at+set_config=lora:app_key:" + _appKEY;
-      //  _serial1.println(command);
-      sendRawCommand(command);
-
-      ret = rk_recvData();
-      if (ret.indexOf("OK") >= 0) {
-        return true;
-      }
-    }
-  }
-
   return false;
 }
 
 bool RAK3172::rk_initABP(String devADDR, String nwksKEY, String appsKEY) {
-  // String command = "";
-  if (devADDR.length() == 8) {
-    _devADDR = devADDR;
-  } else {
-    _serial1.println(F("The parameter devADDR is set incorrectly!"));
-  }
-  if (nwksKEY.length() == 32) {
-    _nwksKEY = nwksKEY;
-  } else {
-    _serial1.println(F("The parameter nwksKEY is set incorrectly!"));
-  }
-  if (appsKEY.length() == 32) {
-    _appsKEY = appsKEY;
-  } else {
-    _serial1.println(F("The parameter appsKEY is set incorrectly!"));
-  }
-  command = "at+set_config=lora:dev_addr:" + _devADDR;
-  sendRawCommand(command);
-  ret = rk_recvData();
-  // _serial1.println(ret);
-  if (ret.indexOf("OK") >= 0) {
-    command = "at+set_config=lora:nwks_key:" + _nwksKEY;
-    sendRawCommand(command);
-    ret = rk_recvData();
-    if (ret.indexOf("OK") >= 0) {
-      command = "at+set_config=lora:apps_key:" + _appsKEY;
-      sendRawCommand(command);
-      ret = rk_recvData();
-      if (ret.indexOf("OK") >= 0) {
-        return true;
-      }
-    }
-  }
   return false;
 }
 
 bool RAK3172::rk_isConfirm(int type) {
-  switch (type) {
-    case 0:
-      sendRawCommand(F("at+set_config=lora:confirm:0"));  //LoRa data send with unconfirm.
-      break;
-    case 1:
-      sendRawCommand(F("at+set_config=lora:confirm:1"));  //LoRa data send with confirm.
-      break;
-    default:
-      return false;
-  }
-  ret = rk_recvData();
-  if (ret.indexOf("OK") >= 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return false;
 }
 
 bool RAK3172::rk_sendData(int port, char *datahex) {
@@ -463,18 +283,11 @@ bool RAK3172::rk_sendP2PData(char *datahex) {
 }
 
 String RAK3172::rk_checkDeviceStatus(void) {
-  sendRawCommand(F("at+get_config=device:status"));
-  ret = rk_recvP2PData();
-  return ret;
+  return false;
 }
 
 bool RAK3172::rk_setUARTConfig(int UartPort, int Baud) {
-  // String command = "";
-  command = "at+set_config=device:uart:" + (String)UartPort + ":" + (String)Baud;
-  //  _serial1.println(command);
-  sendRawCommand(command);
-
-  return true;
+return false;
 }
 
 bool RAK3172::sendRawCommand(String cmd) {

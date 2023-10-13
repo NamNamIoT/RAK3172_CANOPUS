5840643f001420e902c8f914883eb16efd87be5b
Canopus <49629370+NamNamIoT@users.noreply.github.com>
2023-10-13 10:42:55 +0700

    Update Rak3172_Canopus.h
    Define pin

--- Rak3172_Canopus.h
+++ Rak3172_Canopus.h
@@ -3,4 +3,16 @@
 
 #ifdef RAK3172_CANOPUS_H
 
+#define	LED_SEND PA8
+#define LED_SYNC PA0
+#define LED_RECV PB2
+#define VSS_PIN PB5
+#define I2C_SDA PA11
+#define I2C_SCL PA12
+#define mA_PIN PB3
+#define Volt_PIN PB4
+#define VRS_485 -1
+#define PWR_ON LOW
+#define PWR_OFF HIGH
+
 #endif

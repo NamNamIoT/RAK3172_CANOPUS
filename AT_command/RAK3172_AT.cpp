eb2d4c31ba8591520a17408addbefc4355b81ea2
Canopus <49629370+NamNamIoT@users.noreply.github.com>
2024-02-12 21:33:48 +0700

    Update RAK3172_AT.cpp

--- AT_command/RAK3172_AT.cpp
+++ AT_command/RAK3172_AT.cpp
@@ -14,6 +14,7 @@ extern "C" {
 #include "string.h"
 #include "stdlib.h"
 }
+bool frame_error = false;
 String ret = "";
 String command = "";
 /*
@@ -34,31 +35,55 @@ bool RAK3172::rk_switchToRecv() {
   _serial1.print("Enable recv ");
   sendRawCommand(command);
   ret = rk_recvP2PData();
-  if (ret.indexOf("OK") >= 0)
-  {
+  if (ret.indexOf("OK") >= 0) {
     _serial1.println("OK");
     return true;
-  }
-  else {
+  } else {
     _serial1.println("Fail");
     return false;
   }
 }
 
 String RAK3172::rk_getLoRaStatus() {
-  return false;
+  sendRawCommand(F("at+get_config=lora:status"));
+  ret = rk_recvData();
+  ret.trim();
+  return ret;
 }
 
 String RAK3172::rk_getChannelList() {
-  return false;
+  sendRawCommand(F("at+get_config=lora:channel"));
+  ret = rk_recvData();
+  ret.trim();
+  return ret;
 }
 
 bool RAK3172::rk_setRate(int rate) {
-  return false;
+  sendRawCommand("at+set_config=lora:dr:" + (String)rate);
+  ret = rk_recvData();
+  if (ret.indexOf("OK") >= 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 bool RAK3172::rk_setClass(int classMode) {
-  return false;
+  if (classMode > 2) {
+    _serial1.println(F("Parameter error"));
+    return false;
+  }
+  command = "at+set_config=lora:class:" + (String)classMode;
+  sendRawCommand(command);
+  ret = rk_recvData();
+#if defined DEBUG_MODE
+  _serial1.println(ret);
+#endif
+  if (ret.indexOf("OK") >= 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 bool RAK3172::rk_setRegion(int region) {
@@ -100,19 +125,50 @@ bool RAK3172::rk_setRegion(int region) {
       REGION = "US915_Hybrid";
       break;
   }
-return false;
+  _serial1.println("Current work region: " + REGION);
+  sendRawCommand("at+set_config=lora:region:" + REGION);
+  ret = rk_recvData();
+#if defined DEBUG_MODE
+  _serial1.println(ret);
+#endif
+  if (ret.indexOf("OK") >= 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 void RAK3172::rk_sleep(int mode) {
-  return false;
+  switch (mode) {
+    case 1:
+      sendRawCommand(F("at+set_config=device:sleep:1"));
+      break;
+    case 0:
+      sendRawCommand(F("at+set_config=device:sleep:0"));
+      break;
+    default:
+      _serial1.println(F("Parameter error"));
+      break;
+  }
 }
 
 bool RAK3172::rk_setSendinterval(int mode, int value) {
-  return false;
+  if (mode > 2) {
+    _serial1.println("The mode set Error,the mode is '0','1' or '2'.");
+    return false;
+  }
+
+  sendRawCommand("at+set_config=lora:send_interval:" + (String)mode + ":" + (String)value);
+  ret = rk_recvData();
+  if (ret.indexOf("OK") >= 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 void RAK3172::rk_reset(void) {
-  return false;
+  sendRawCommand(F("at+set_config=device:restart"));
 }
 
 bool RAK3172::rk_setWorkingMode(int mode) {
@@ -139,29 +195,150 @@ bool RAK3172::rk_setWorkingMode(int mode) {
 }
 
 bool RAK3172::rk_setJoinMode(int mode) {
-  return false;
+  switch (mode) {
+    case 0:
+      sendRawCommand(F("at+set_config=lora:join_mode:0"));  //join Network through OTAA mode.
+      break;
+    case 1:
+      sendRawCommand(F("at+set_config=lora:join_mode:1"));  //join Network through ABP mode.
+      break;
+    default:
+      return false;
+  }
+  ret = rk_recvData();
+  if (ret.indexOf("OK") >= 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 bool RAK3172::rk_joinLoRaNetwork(int timeout) {
+  String command = "at+join";
+  //  _serial1.println(command);
+  sendRawCommand(command);
+  ret = rk_recvData();
+  if (ret != NULL) {
+    if (ret.indexOf("OK") >= 0)
+      return true;
+    else if (ret.indexOf("ERROR") >= 0)
+      return false;
+  }
+  for (int i = 0; i <= timeout / 2; i++) {
+    ret = rk_recvData();
+    if (ret != NULL) {
+      _serial1.print("<- " + ret);
+      if (ret.indexOf("OK") >= 0)
+        return true;
+      // else if(ret.indexOf("ERROR") >= 0)
+      //   return false;
+    }
+  }
+
   return false;
 }
 
 bool RAK3172::rk_initOTAA(String devEUI, String appEUI, String appKEY) {
+  // String command = "";
+  if (devEUI.length() == 16) {
+    _devEUI = devEUI;
+  } else {
+    _serial1.println("The parameter devEUI is set incorrectly!");
+  }
+  if (appEUI.length() == 16) {
+    _appEUI = appEUI;
+  } else {
+    _serial1.println("The parameter appEUI is set incorrectly!");
+  }
+  if (appKEY.length() == 32) {
+    _appKEY = appKEY;
+  } else {
+    _serial1.println("The parameter appKEY is set incorrectly!");
+  }
+
+  command = "at+set_config=lora:dev_eui:" + _devEUI;
+  //  _serial1.println(command);
+  sendRawCommand(command);
+  ret = rk_recvData();
+  if (ret.indexOf("OK") >= 0) {
+    command = "at+set_config=lora:app_eui:" + _appEUI;
+    //  _serial1.println(command);
+    sendRawCommand(command);
+
+    ret = rk_recvData();
+    if (ret.indexOf("OK") >= 0) {
+      command = "at+set_config=lora:app_key:" + _appKEY;
+      //  _serial1.println(command);
+      sendRawCommand(command);
+
+      ret = rk_recvData();
+      if (ret.indexOf("OK") >= 0) {
+        return true;
+      }
+    }
+  }
+
   return false;
 }
 
 bool RAK3172::rk_initABP(String devADDR, String nwksKEY, String appsKEY) {
+  // String command = "";
+  if (devADDR.length() == 8) {
+    _devADDR = devADDR;
+  } else {
+    _serial1.println(F("The parameter devADDR is set incorrectly!"));
+  }
+  if (nwksKEY.length() == 32) {
+    _nwksKEY = nwksKEY;
+  } else {
+    _serial1.println(F("The parameter nwksKEY is set incorrectly!"));
+  }
+  if (appsKEY.length() == 32) {
+    _appsKEY = appsKEY;
+  } else {
+    _serial1.println(F("The parameter appsKEY is set incorrectly!"));
+  }
+  command = "at+set_config=lora:dev_addr:" + _devADDR;
+  sendRawCommand(command);
+  ret = rk_recvData();
+  // _serial1.println(ret);
+  if (ret.indexOf("OK") >= 0) {
+    command = "at+set_config=lora:nwks_key:" + _nwksKEY;
+    sendRawCommand(command);
+    ret = rk_recvData();
+    if (ret.indexOf("OK") >= 0) {
+      command = "at+set_config=lora:apps_key:" + _appsKEY;
+      sendRawCommand(command);
+      ret = rk_recvData();
+      if (ret.indexOf("OK") >= 0) {
+        return true;
+      }
+    }
+  }
   return false;
 }
 
 bool RAK3172::rk_isConfirm(int type) {
-  return false;
+  switch (type) {
+    case 0:
+      sendRawCommand(F("at+set_config=lora:confirm:0"));  //LoRa data send with unconfirm.
+      break;
+    case 1:
+      sendRawCommand(F("at+set_config=lora:confirm:1"));  //LoRa data send with confirm.
+      break;
+    default:
+      return false;
+  }
+  ret = rk_recvData();
+  if (ret.indexOf("OK") >= 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 bool RAK3172::rk_sendData(int port, char *datahex) {
-  // String command = "";
   command = "at+send=lora:" + (String)port + ":" + datahex;
-  // _serial1.println(command);
   sendRawCommand(command);
 
   return true;
@@ -170,8 +347,6 @@ bool RAK3172::rk_sendData(int port, char *datahex) {
 String RAK3172::rk_recvData(void) {
   _serial.setTimeout(100);
   ret = _serial.readStringUntil('\0');
-  // ret.trim();
-  // _serial1.println(ret);
   return ret;
 }
 
@@ -190,85 +365,75 @@ bool RAK3172::rk_initP2P(String FREQ, int SF, int BW, int CR, int PRlen, int PWR
   _serial1.print("Set SF. ");
   sendRawCommand(command);
   ret = rk_recvP2PData();
-  /*
   if (ret.indexOf("OK") >= 0)
     _serial1.println("OK");
   else
     _serial1.println("Fail");
-  */
 
   command = "AT+PBW=" + String(BW);
   _serial1.print("Set BW. ");
   sendRawCommand(command);
   ret = rk_recvP2PData();
-  /*
   if (ret.indexOf("OK") >= 0)
     _serial1.println("OK");
   else
     _serial1.println("Fail");
-  */
 
   command = "AT+PCR=" + String(CR);
   _serial1.print("Set CR. ");
   sendRawCommand(command);
   ret = rk_recvP2PData();
-  /*
   if (ret.indexOf("OK") >= 0)
     _serial1.println("OK");
   else
     _serial1.println("Fail");
-  */
 
   command = "AT+PPL=" + String(PRlen);
   _serial1.print("Set PRlen. ");
   sendRawCommand(command);
   ret = rk_recvP2PData();
-  /*
   if (ret.indexOf("OK") >= 0)
     _serial1.println("OK");
   else
     _serial1.println("Fail");
-  */
 
   command = "AT+PTP=" + String(PWR);
   _serial1.print("Set power. ");
   sendRawCommand(command);
   ret = rk_recvP2PData();
-  /*
   if (ret.indexOf("OK") >= 0)
     _serial1.println("OK");
   else
     _serial1.println("Fail");
-  */
   return true;
 }
 
 String RAK3172::rk_recvP2PData(void) {
+  frame_error = false;
   ret = _serial.readStringUntil('\0');
   ret.trim();
-
-  return ret;
+  if (ret.indexOf("ERROR") >= 0)
+    frame_error = true;
+    return ret;
 }
 
 bool RAK3172::rk_sendP2PData(char *datahex) {
   command = "AT+PRECV=0";
-  _serial1.print("Enable send ");
   sendRawCommand(command);
   ret = rk_recvP2PData();
-  if (ret.indexOf("OK") >= 0)
-    _serial1.println("OK");
-  else {
-    _serial1.println("Fail");
+  if (ret.indexOf("OK") >= 0) {
+
+  } else {
     return false;
   }
-
+  delay(10);
   String DATAHEX = datahex;
   String command = "AT+PSEND=" + DATAHEX;
   sendRawCommand(command);
-
+  delay(10);
   while (!_serial.available()) {
     _serial1.println(".");
-    delay(100);
+    delay(10);
   }
   ret = rk_recvP2PData();
   if (ret.indexOf("OK") >= 0) {
@@ -278,24 +443,30 @@ bool RAK3172::rk_sendP2PData(char *datahex) {
     _serial1.println(ret);
     return false;
   }
-
   return true;
 }
 
 String RAK3172::rk_checkDeviceStatus(void) {
-  return false;
+  sendRawCommand(F("at+get_config=device:status"));
+  ret = rk_recvP2PData();
+  return ret;
 }
 
 bool RAK3172::rk_setUARTConfig(int UartPort, int Baud) {
-return false;
+  // String command = "";
+  command = "at+set_config=device:uart:" + (String)UartPort + ":" + (String)Baud;
+  //  _serial1.println(command);
+  sendRawCommand(command);
+
+  return true;
 }
 
 bool RAK3172::sendRawCommand(String cmd) {
   while (_serial.available()) {
     _serial.read();
   }
-  _serial1.println(cmd);
+  //_serial1.println(cmd);
   _serial.println(cmd);
-  delay(100);
+  delay(10);
   return true;
 }

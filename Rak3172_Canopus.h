0296297bad3d914f211a87e5767c57e9daa7ff82
Canopus <thanhnamlt5@gmail.com>
2024-05-30 01:25:49 +0700

    Update Rak3172_Canopus.h

--- Rak3172_Canopus.h
+++ Rak3172_Canopus.h
@@ -5,33 +5,28 @@
 #define V3
 #ifdef RAK3172_CANOPUS_H
 
-#define LED_YELLOW PB2
-#define LED_RECV PB2
-#define LED_BLUE PA8
-#define LED_SEND PA8
+#define LED_RECV PB2 //LED_YELLOW
+#define LED_SEND PA8 //LED_BLUE
 #define I2C_SDA PA11
 #define I2C_SCL PA12
 #define Volt_PIN PB4
 
 #ifdef V1
-#define LED_RED PA0
-#define LED_SYNC PA0
+#define LED_SYNC PA0 //LED_RED
 #define mA_PIN PB3
 #define V_SS3 PB5
 #define V_SS5 PB12
 #endif
 
 #ifdef V2
-#define LED_RED PA10
-#define LED_SYNC PA10
+#define LED_SYNC PA10 //LED_RED
 #define mA_PIN PB3
 #define V_SS3 PB5
 #define V_SS5 PA1
 #endif
 
 #ifdef V3
-#define LED_RED PA9
-#define LED_SYNC PA9
+#define LED_SYNC PA9 //LED_RED
 #define AI2_PIN PA10
 #define AI1_PIN PA15
 #define V_SS3 PB5
